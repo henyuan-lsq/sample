9ec64252b86f003ee558ba3dd3dd379d5f88e7b3
henyuan-lsq <lsqfordev@gmail.com>
2017-05-16 17:01:21 +0800

    无语了

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Sample') App</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+  <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+  </header>
 @yield('content')
 </body>
 </html>
